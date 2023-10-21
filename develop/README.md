3daba44a35545b1173b3d2476ccc3664e2fa4ce2
bkness <kbrandon863@gmail.com>
2023-10-21 15:54:40 -0700

    added inquirer and fs to project, created a list of objects with a type name and message. then created a function to take the date and create a readme with. Set up a different spot for my readme that is created so I can keep this projects original readme

---
+++ develop/README.md
@@ -0,0 +1,32 @@
+# readme generator
+ ![Github license](https://img.shields.io/badge/license-APACHE%202.0-blue.svg)
+
+## Description
+creates a readme for the user
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Test](#test)
+- [Contributing](#contributing)
+ - [License](#license)
+- [Questions](#questions)
+
+## Installation 
+npm i
+
+## Usage
+
+
+## Test 
+npm test
+
+## Contributing
+Fork the project and open a pull request with your new code
+
+ ## License
+This project is licensed under the APACHE 2.0 license
+
+## Questions
+If you have any questions you can email me at kbrandon863@gmail.com if you want to see more of my work, visit my GitHub at [bkness](https://github.com/bkness)
--- develop/utils/generatedMarkdown.js
+++ develop/utils/generatedMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license !== 'None')
+     return ` ![Github license](https://img.shields.io/badge/license-${license.replace(/ /g,'%20')}-blue.svg)`;
+    return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license !== 'None')
+    return ` - [License](#license)`;
+   return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license !== 'None')
+    return ` ## License\nThis project is licensed under the ${license} license`;
+   return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+${data.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Test](#test)
+- [Contributing](#contributing)
+${renderLicenseLink(data.license)}
+- [Questions](#questions)
+
+## Installation 
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Test 
+${data.test}
+
+## Contributing
+${data.contributing}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+If you have any questions you can email me at ${data.email} if you want to see more of my work, visit my GitHub at [${data.github}](https://github.com/${data.github})
 `;
 }
 
